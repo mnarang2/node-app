9368eb0994d1b7262fabdc5f6f2cf23b6843e06b
Ubuntu <maninder.narang2@gmail.com>
2019-09-24 13:06:34 +0000

    new code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hey Anchal, this is our CI/CD Pipeline');
+  res.send('Hey Anchal, this is our CI/CD Pipeline for node deployment');
 });
 
 app.listen(process.env.PORT || 3000);
