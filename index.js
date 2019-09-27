0e797cb5b67b368c8e376bff52c0062d3ad8dd39
mnarang2 <32914041+mnarang2@users.noreply.github.com>
2019-09-27 12:31:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Anchal, this is our CI/CD Pipleline using Jenkins.');
+  res.send('Hello Anchal, this is our CI/CD Pipleline using Jenkins Deployments.');
 });
 
 app.listen(process.env.PORT || 3000);
